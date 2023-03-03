483e690c2b96666d648ed15ff1914fd80e25f842
Daniel Firth <dan.firth@homotopic.tech>
2023-03-03 12:51:49 +0000

    dhall-docs: init at 1.0.10

---
+++ pkgs/html-entity-map.nix
@@ -0,0 +1,29 @@
+{ mkDerivation, base, criterion, lib, text, unordered-containers }:
+mkDerivation {
+  pname = "html-entity-map";
+  version = "0.1.0.0";
+  sha256 = "983600c33e8515e6ca31742d25490fb5a7be02503331963621b0ba5cd70d344c";
+  revision = "5";
+  editedCabalFile = "1776z73bs1v8z3qwpjvqqmzdlj6gm8qba93f41irhn7swdppfq72";
+  isLibrary = true;
+  isExecutable = false;
+  enableSeparateDataOutput = false;
+  libraryHaskellDepends = [ base text unordered-containers ];
+  benchmarkHaskellDepends = [
+    base
+    criterion
+    text
+    unordered-containers
+  ];
+  enableLibraryProfiling = true;
+  enableExecutableProfiling = true;
+  doHaddock = false;
+  jailbreak = true;
+  doCheck = false;
+  doBenchmark = false;
+  hyperlinkSource = false;
+  homepage = "https://github.com/mrkkrp/html-entity-map";
+  description = "Map from HTML5 entity names to the corresponding Unicode text";
+  license = lib.licenses.bsd3;
+  broken = false;
+}
---
+++ pkgs/modern-uri.nix
@@ -0,0 +1,78 @@
+{ mkDerivation
+, QuickCheck
+, base
+, bytestring
+, containers
+, contravariant
+, criterion
+, deepseq
+, exceptions
+, hashable
+, hspec
+, hspec-discover
+, hspec-megaparsec
+, lib
+, megaparsec
+, mtl
+, profunctors
+, reflection
+, tagged
+, template-haskell
+, text
+, weigh
+}:
+mkDerivation {
+  pname = "modern-uri";
+  version = "0.3.6.0";
+  sha256 = "cf3c5b2cbbab9b8f3c171488ff8056dd1c37de011301090345689fc7138d43d2";
+  isLibrary = true;
+  isExecutable = false;
+  enableSeparateDataOutput = false;
+  libraryHaskellDepends = [
+    base
+    bytestring
+    containers
+    contravariant
+    deepseq
+    exceptions
+    hashable
+    megaparsec
+    mtl
+    profunctors
+    QuickCheck
+    reflection
+    tagged
+    template-haskell
+    text
+  ];
+  testHaskellDepends = [
+    base
+    bytestring
+    hspec
+    hspec-megaparsec
+    megaparsec
+    QuickCheck
+    text
+  ];
+  testToolDepends = [ hspec-discover ];
+  benchmarkHaskellDepends = [
+    base
+    bytestring
+    criterion
+    deepseq
+    megaparsec
+    text
+    weigh
+  ];
+  enableLibraryProfiling = true;
+  enableExecutableProfiling = true;
+  doHaddock = false;
+  jailbreak = true;
+  doCheck = false;
+  doBenchmark = false;
+  hyperlinkSource = false;
+  homepage = "https://github.com/mrkkrp/modern-uri";
+  description = "Modern library for working with URIs";
+  license = lib.licenses.bsd3;
+  broken = false;
+}
