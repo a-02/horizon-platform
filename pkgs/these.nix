f8480dd42f7d46442a9aa328f4c4042755769664
Daniel Firth <dan.firth@homotopic.tech>
2023-03-16 07:18:57 +0000

    these: 1.1.1.1 -> 1.2

--- pkgs/these.nix
+++ pkgs/these.nix
@@ -1,14 +1,27 @@
-{ mkDerivation, assoc, base, binary, deepseq, hashable, lib }:
+{ mkDerivation
+, assoc
+, base
+, binary
+, deepseq
+, foldable1-classes-compat
+, hashable
+, lib
+}:
 mkDerivation {
   pname = "these";
-  version = "1.1.1.1";
-  sha256 = "d798c9f56e17def441e8f51e54cc11afdb3e76c6a9d1e9ee154e9a78da0bf508";
-  revision = "6";
-  editedCabalFile = "12ll5l8m482qkb8zn79vx51bqlwc89fgixf8jv33a32b4qzc3499";
+  version = "1.2";
+  sha256 = "a8ed7174b7f790764360f48aab72fea382a6093ed369c7bc6881fae1abacd4b8";
   isLibrary = true;
   isExecutable = false;
   enableSeparateDataOutput = false;
-  libraryHaskellDepends = [ assoc base binary deepseq hashable ];
+  libraryHaskellDepends = [
+    assoc
+    base
+    binary
+    deepseq
+    foldable1-classes-compat
+    hashable
+  ];
   enableLibraryProfiling = true;
   enableExecutableProfiling = true;
   doHaddock = false;
