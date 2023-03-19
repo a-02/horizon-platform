743d09b33a554e31705ef99661353d9b916846ac
Daniel Firth <dan.firth@homotopic.tech>
2023-03-19 22:43:04 +0000

    hie-bios: 0.12.0 -> 0.11.0

--- pkgs/hie-bios.nix
+++ pkgs/hie-bios.nix
@@ -1,33 +1,34 @@
 { mkDerivation, aeson, base, base16-bytestring, bytestring
-, co-log-core, conduit, conduit-extra, cryptohash-sha1, deepseq
-, directory, exceptions, extra, file-embed, filepath, ghc, lib
-, optparse-applicative, prettyprinter, tasty
-, tasty-expected-failure, tasty-hunit, template-haskell, temporary
-, text, time, transformers, unix-compat, unordered-containers, yaml
+, co-log-core, conduit, conduit-extra, containers, cryptohash-sha1
+, deepseq, directory, exceptions, extra, file-embed, filepath, ghc
+, lib, optparse-applicative, prettyprinter, process, tagged, tasty
+, tasty-expected-failure, tasty-hunit, temporary, text, time
+, transformers, unix-compat, unordered-containers, vector, yaml
 }:
 mkDerivation {
   pname = "hie-bios";
-  version = "0.12.0";
-  sha256 = "64eb00bb32dec259d0b92c63440bd29c72621683824693c082f643e398db2688";
-  revision = "1";
-  editedCabalFile = "18cmkzrq45schidxrn571b0x6x4vlmwkpzq0nwm97p4gsqvvwj48";
+  version = "0.11.0";
+  sha256 = "9708bf4309e904771bf590dd4e683e0b5c18e486ed499be8ceff5119a25f0eef";
+  revision = "3";
+  editedCabalFile = "0axmy522cfk92c5hwrqhkqswlxlm26m821b64rnh32666sxfdhy1";
   isLibrary = true;
   isExecutable = true;
   enableSeparateDataOutput = false;
   libraryHaskellDepends = [
     aeson base base16-bytestring bytestring co-log-core conduit
-    conduit-extra cryptohash-sha1 deepseq directory exceptions extra
-    file-embed filepath ghc prettyprinter template-haskell temporary
-    text time transformers unix-compat unordered-containers yaml
+    conduit-extra containers cryptohash-sha1 deepseq directory
+    exceptions extra file-embed filepath ghc prettyprinter process
+    temporary text time transformers unix-compat unordered-containers
+    vector yaml
   ];
   executableHaskellDepends = [
-    base co-log-core directory filepath optparse-applicative
+    base co-log-core directory filepath ghc optparse-applicative
     prettyprinter
   ];
   testHaskellDepends = [
-    aeson base co-log-core directory extra filepath ghc prettyprinter
-    tasty tasty-expected-failure tasty-hunit temporary transformers
-    yaml
+    aeson base directory extra filepath ghc tagged tasty
+    tasty-expected-failure tasty-hunit temporary text transformers
+    unordered-containers yaml
   ];
   enableLibraryProfiling = true;
   enableExecutableProfiling = true;
