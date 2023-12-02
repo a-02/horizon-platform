5514c20e695d4533ba38d5f05e26d0e9f5bf938d
Daniel Firth <dan.firth@horizon-haskell.net>
2023-12-02 23:58:41 +0000

    xml-conduit-writer: patch

--- configuration/common.nix
+++ configuration/common.nix
@@ -21,6 +21,16 @@ final: prev: {
 
   text-icu = prev.callPackage ../pkgs/text-icu.nix { icu-i18n = pkgs.icu; };
 
+  xml-conduit-writer = overrideCabal
+    prev.xml-conduit-writer
+    (_:
+      {
+        patches = [
+          ./patches/xml-conduit-writer-01.patch
+      ];
+    });
+
+
   X11-xft = addPkgconfigDepends prev.X11-xft [ pkgs.expat pkgs.xorg.libXau pkgs.xorg.libXdmcp ];
 
 }
